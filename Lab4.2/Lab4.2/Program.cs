70ce1c9219b0251be56798db5faac52067a2634b
mihawrmm <128784469+mihawrmm@users.noreply.github.com>
2024-03-02 13:11:35 +0200

    Add files via upload

---
+++ Lab4.2/Lab4.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array2D = {
+            { 1, 2, 3 },
+            { 4, -1, -2 },
+            { -3, -4, -5 }
+        };
+
+        Console.WriteLine("Двовимірний масив:");
+        for (int i = 0; i < array2D.GetLength(0); i++)
+        {
+            for (int j = 0; j < array2D.GetLength(1); j++)
+            {
+                Console.Write(array2D[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        int lowerRight = array2D[array2D.GetLength(0) - 1, array2D.GetLength(1) - 1];
+        int lowerLeft = array2D[array2D.GetLength(0) - 1, 0];
+        Console.WriteLine($"Елемент у нижньому правому куті: {lowerRight}");
+        Console.WriteLine($"Елемент у нижньому лівому куті: {lowerLeft}");
+        Console.WriteLine($"Менший елемент: {(lowerRight < lowerLeft ? lowerRight : lowerLeft)}");
+
+        int upperRight = array2D[0, array2D.GetLength(1) - 1];
+        Console.WriteLine($"Елемент у верхньому правому куті: {upperRight}");
+        Console.WriteLine($"Менший елемент: {(upperRight < lowerLeft ? upperRight : lowerLeft)}");
+    }
+}
+
