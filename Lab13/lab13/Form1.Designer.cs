f65e5238d371dea9621f81349450e6c9ec38d4d0
mihawrmm <128784469+mihawrmm@users.noreply.github.com>
2024-05-17 19:39:45 +0300

    Add files via upload

---
+++ Lab13/lab13/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Location = new System.Drawing.Point(799, 305);
+            this.listBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(179, 160);
+            this.listBox7.TabIndex = 39;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(347, 357);
+            this.listBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(245, 108);
+            this.listBox6.TabIndex = 38;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(387, 470);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(177, 23);
+            this.button7.TabIndex = 37;
+            this.button7.Text = "Перегляд текстового файлу";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(76, 445);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 27);
+            this.button6.TabIndex = 36;
+            this.button6.Text = "Вивести зображення";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 223);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 218);
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(606, 281);
+            this.listBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(189, 160);
+            this.listBox5.TabIndex = 34;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(640, 491);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 19);
+            this.button5.TabIndex = 33;
+            this.button5.Text = "Пошук за файлом";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(640, 468);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 19);
+            this.button4.TabIndex = 32;
+            this.button4.Text = "Пошук за каталогом";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(640, 445);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(189, 98);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 21);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "Інформація про файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 65);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 28);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "Інформація про каталог";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(346, 240);
+            this.listBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(245, 108);
+            this.listBox4.TabIndex = 29;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(8, 97);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(158, 108);
+            this.listBox3.TabIndex = 28;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, -26);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Введіть існуючий шлях";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(346, 11);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(632, 225);
+            this.listBox2.TabIndex = 24;
+            this.listBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-46, -26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Список дисків";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(37, 11);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(125, 82);
+            this.listBox1.TabIndex = 21;
+            this.listBox1.MouseCaptureChanged += new System.EventHandler(this.listBox1_MouseCaptureChanged);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(801, 486);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(177, 27);
+            this.button8.TabIndex = 40;
+            this.button8.Text = "Переглянути атрибути безпеки";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 522);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Lab13/lab13/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace lab13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDrives();
+        }
+
+        private void LoadDrives()
+        {
+            // Отримання списку доступних дисків
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Додавання дисків до ListBox
+            foreach (DriveInfo drive in drives)
+            {
+                listBox1.Items.Add(drive.Name);
+            }
+        }
+
+        
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Перевіряємо, чи натиснута клавіша Enter
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string path = textBox1.Text;
+
+                if (Directory.Exists(path))
+                {
+                    listBox2.Items.Clear();
+
+                    string[] directories = Directory.GetDirectories(path);
+                    foreach (string directory in directories)
+                    {
+                        listBox2.Items.Add(directory);
+                    }
+
+                    string[] files = Directory.GetFiles(path);
+                    foreach (string file in files)
+                    {
+                        listBox2.Items.Add(file);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Шлях не існує");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string currentPath = textBox1.Text;
+            string parentPath = Path.GetDirectoryName(currentPath);
+
+            if (!string.IsNullOrEmpty(parentPath))
+            {
+                textBox1.Text = parentPath;
+
+                listBox2.Items.Clear();
+
+                string[] directories = Directory.GetDirectories(parentPath);
+                foreach (string directory in directories)
+                {
+                    listBox2.Items.Add(directory);
+                }
+
+                string[] files = Directory.GetFiles(parentPath);
+                foreach (string file in files)
+                {
+                    listBox2.Items.Add(file);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Це кореневий каталог");
+            }
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox2.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                string selectedItem = listBox2.Items[index].ToString();
+
+                if (Directory.Exists(selectedItem))
+                {
+                    textBox1.Text = selectedItem;
+                    listBox2.Items.Clear();
+
+                    string[] directories = Directory.GetDirectories(selectedItem);
+                    foreach (string directory in directories)
+                    {
+                        listBox2.Items.Add(directory);
+                    }
+
+                    string[] files = Directory.GetFiles(selectedItem);
+                    foreach (string file in files)
+                    {
+                        listBox2.Items.Add(file);
+                    }
+                }
+            }
+        }
+
+        private void listBox1_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            int index = listBox1.IndexFromPoint(listBox1.PointToClient(MousePosition));
+            if (index != ListBox.NoMatches)
+            {
+                // Очистити listBox3 перед виведенням властивостей нового вибраного диску
+                listBox3.Items.Clear();
+
+                // Отримати список доступних дисків
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                // Отримати вибраний диск
+                DriveInfo selectedDrive = drives[index];
+
+                // Вивести властивості в listBox3
+                listBox3.Items.Add($"Назва: {selectedDrive.Name}");
+                listBox3.Items.Add($"Тип: {selectedDrive.DriveType}");
+
+                if (selectedDrive.IsReady)
+                {
+                    double totalSize = selectedDrive.TotalSize;
+                    double totalFreeSpace = selectedDrive.TotalFreeSpace;
+
+                    listBox3.Items.Add($"Об’єм диску: {Math.Round(totalSize / (1024 * 1024 * 1024), 2)} Гб");
+                    listBox3.Items.Add($"Вільний простір: {Math.Round(totalFreeSpace / (1024 * 1024 * 1024), 2)} Гб");
+                    listBox3.Items.Add($"Мітка диску: {selectedDrive.VolumeLabel}");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox2.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                // Очистити listBox3 перед виведенням властивостей нового вибраного каталогу
+                listBox4.Items.Clear();
+
+                // Отримати вибраний каталог
+                string selectedDirectory = listBox2.SelectedItem.ToString();
+
+                // Отримати інформацію про каталог
+                DirectoryInfo directoryInfo = new DirectoryInfo(selectedDirectory);
+
+                // Вивести властивості в listBox3
+                listBox4.Items.Add($"Назва каталогу: {directoryInfo.Name}");
+                listBox4.Items.Add($"Повний шлях: {directoryInfo.FullName}");
+                listBox4.Items.Add($"Дата створення: {directoryInfo.CreationTime}");
+                listBox4.Items.Add($"Остання модифікація: {directoryInfo.LastWriteTime}");
+
+                // Отримати список файлів у каталозі
+                FileInfo[] files = directoryInfo.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    listBox4.Items.Add($"Файл: {file.Name}");
+                }
+
+                // Отримати список підкаталогів у каталозі
+                DirectoryInfo[] directories = directoryInfo.GetDirectories();
+                foreach (DirectoryInfo subdirectory in directories)
+                {
+                    listBox4.Items.Add($"Підкаталог: {subdirectory.Name}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Виберіть каталог зі списку.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox2.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                listBox4.Items.Clear();
+
+                string selectedFile = listBox2.SelectedItem.ToString();
+
+                FileInfo fileInfo = new FileInfo(selectedFile);
+
+                listBox4.Items.Clear();
+                listBox4.Items.Add($"Назва файлу: {fileInfo.Name}");
+                listBox4.Items.Add($"Повний шлях: {fileInfo.FullName}");
+                listBox4.Items.Add($"Розмір: {fileInfo.Length} байт");
+                listBox4.Items.Add($"Дата створення: {fileInfo.CreationTime}");
+                listBox4.Items.Add($"Остання модифікація: {fileInfo.LastWriteTime}");
+            }
+            else
+            {
+                MessageBox.Show("Виберіть файл зі списку.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox2.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                // Очистити listBox3 перед виведенням властивостей нового вибраного каталогу
+                listBox5.Items.Clear();
+
+                string selectedDirectory = listBox2.Items[selectedIndex].ToString();
+
+                // Отримати інформацію про каталог
+                DirectoryInfo directoryInfo = new DirectoryInfo(selectedDirectory);
+
+                string pattern = textBox2.Text; // Отримуємо шаблон рядка з TextBox
+
+                DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
+
+                foreach (DirectoryInfo subDirectory in subDirectories)
+                {
+                    if (Regex.IsMatch(subDirectory.Name, pattern))
+                    {
+                        listBox5.Items.Add(subDirectory.FullName); // Додаємо елементи в ListBox
+                    }
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox2.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                // Очистити listBox3 перед виведенням властивостей нового вибраного каталогу
+                listBox5.Items.Clear();
+
+                string selectedDirectory = listBox2.Items[selectedIndex].ToString();
+
+                // Отримати інформацію про каталог
+                DirectoryInfo directoryInfo = new DirectoryInfo(selectedDirectory);
+
+                string pattern = textBox2.Text; // Отримуємо шаблон рядка з TextBox
+
+                FileInfo[] files = directoryInfo.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    if (Regex.IsMatch(file.Name, pattern))
+                    {
+                        listBox5.Items.Add(file.FullName); // Додаємо елементи в ListBox
+                    }
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+
+            int selectedIndex = listBox2.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                string selectedFile = listBox2.Items[selectedIndex].ToString();
+
+                // Перевіряємо, чи вибраний файл є графічним файлом
+                if (IsImageFile(selectedFile))
+                {
+                    // Завантажуємо зображення з файлу і відображаємо його в pictureBox
+                    Image image = Image.FromFile(selectedFile);
+                    pictureBox1.Image = image;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+                else
+                {
+                    MessageBox.Show("Даний файл не є зображенням");
+                }
+            }
+        }
+
+        private bool IsImageFile(string fileName)
+        {
+            // Перевіряємо розширення файлу, щоб визначити, чи є він графічним файлом
+            string extension = System.IO.Path.GetExtension(fileName).ToLower();
+            return extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif" || extension == ".bmp";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox6.Items.Clear();
+
+            int selectedIndex = listBox2.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                string selectedFile = listBox2.Items[selectedIndex].ToString();
+
+                // Перевіряємо, чи вибраний файл є графічним файлом
+                if (IsTextFile(selectedFile))
+                {
+                    string fileContent = File.ReadAllText(selectedFile);
+                    listBox6.Items.Add(fileContent);
+                }
+                else
+                {
+                    MessageBox.Show("Даний файл не є зображенням");
+                }
+            }
+        }
+
+        private bool IsTextFile(string fileName)
+        {
+            string extension = Path.GetExtension(fileName).ToLower();
+            return extension == ".txt" || extension == ".log" || extension == ".csv" || extension == ".xml" || extension == ".json";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox2.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                string selectedPath = listBox2.SelectedItem.ToString();
+
+                // Отримуємо атрибути безпеки для вказаного шляху
+                FileSecurity fileSecurity = File.GetAccessControl(selectedPath);
+                AuthorizationRuleCollection accessRules = fileSecurity.GetAccessRules(true, true, typeof(System.Security.Principal.SecurityIdentifier));
+
+                // Очищаємо список і додаємо кожен результат окремим елементом
+                foreach (var rule in accessRules)
+                {
+                    listBox7.Items.Add(rule.ToString());
+                }
+            }
+        }
+    }
+}
