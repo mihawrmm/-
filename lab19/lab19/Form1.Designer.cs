71d7f15d667fa1fa3ae3899ce714ccc046600f17
mihawrmm <128784469+mihawrmm@users.noreply.github.com>
2024-06-04 18:20:05 +0300

    Add files via upload

---
+++ lab19/lab19/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace lab19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxHWInfo = new System.Windows.Forms.ListBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxHWInfo
+            // 
+            this.listBoxHWInfo.FormattingEnabled = true;
+            this.listBoxHWInfo.Location = new System.Drawing.Point(11, 11);
+            this.listBoxHWInfo.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxHWInfo.Name = "listBoxHWInfo";
+            this.listBoxHWInfo.Size = new System.Drawing.Size(477, 329);
+            this.listBoxHWInfo.TabIndex = 3;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(11, 344);
+            this.LoadButton.Margin = new System.Windows.Forms.Padding(2);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(477, 27);
+            this.LoadButton.TabIndex = 2;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 381);
+            this.Controls.Add(this.listBoxHWInfo);
+            this.Controls.Add(this.LoadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxHWInfo;
+        private System.Windows.Forms.Button LoadButton;
+    }
+}
+
---
+++ lab19/lab19/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Management;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            GetAndDisplayHardwareInformation();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAndDisplayHardwareInformation();
+        }
+
+        private List<string> GetHardwareInfo(string win32Class, string classItemField)
+        {
+            List<string> result = new List<string>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + win32Class);
+
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    if (obj[classItemField] != null)
+                    {
+                        result.Add(obj[classItemField].ToString().Trim());
+                    }
+                    else
+                    {
+                        result.Add("Немає даних");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при отриманні інформації: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result;
+
+        }
+
+        private void GetAndDisplayHardwareInformation()
+        {
+
+            ClearInformation();
+
+            DisplayHardwareInfo("Процесор:", GetHardwareInfo("Win32_Processor", "Name"));
+            DisplayHardwareInfo("Виробник:", GetHardwareInfo("Win32_Processor", "Manufacturer"));
+            DisplayHardwareInfo("Опис:", GetHardwareInfo("Win32_Processor", "Description"));
+
+            DisplayHardwareInfo("Відеокарта:", GetHardwareInfo("Win32_VideoController", "Name"));
+            DisplayHardwareInfo("Видеопроцесор:", GetHardwareInfo("Win32_VideoController", "VideoProcessor"));
+            DisplayHardwareInfo("Версія драйверу:", GetHardwareInfo("Win32_VideoController", "DriverVersion"));
+            DisplayHardwareInfo("Об’єм пам’яти (в байтах):", GetHardwareInfo("Win32_VideoController", "AdapterRAM"));
+
+            DisplayHardwareInfo("Назва DVD:", GetHardwareInfo("Win32_CDROMDrive", "Name"));
+            DisplayHardwareInfo("Буква DVD:", GetHardwareInfo("Win32_CDROMDrive", "Drive"));
+
+            DisplayHardwareInfo("Жорстикий диск:", GetHardwareInfo("Win32_DiskDrive", "Caption"));
+            DisplayHardwareInfo("Об’єм (в байтах):", GetHardwareInfo("Win32_DiskDrive", "Size"));
+
+            DisplayHardwareInfo("Материнська плата:", GetHardwareInfo("Win32_BaseBoard", "Product"));
+            DisplayHardwareInfo("Виробник материнської плати:", GetHardwareInfo("Win32_BaseBoard", "Manufacturer"));
+            DisplayHardwareInfo("Серійний номер материнської плати:", GetHardwareInfo("Win32_BaseBoard", "SerialNumber"));
+
+            DisplayHardwareInfo("Мережева карта:", GetHardwareInfo("Win32_NetworkAdapter", "Name"));
+
+            DisplayHardwareInfo("BIOS:", GetHardwareInfo("Win32_BIOS", "Caption"));
+            DisplayHardwareInfo("Версія BIOS:", GetHardwareInfo("Win32_BIOS", "Version"));
+            DisplayHardwareInfo("Виробник BIOS:", GetHardwareInfo("Win32_BIOS", "Manufacturer"));
+        }
+
+        private void DisplayHardwareInfo(string info, List<string> result)
+        {
+
+            if (!string.IsNullOrEmpty(info))
+            {
+                listBoxHWInfo.Items.Add(info);
+            }
+
+            if (result.Count > 0)
+            {
+                foreach (string item in result)
+                {
+                    listBoxHWInfo.Items.Add(item);
+                }
+            }
+
+            listBoxHWInfo.Items.Add(Environment.NewLine);
+        }
+
+        private void ClearInformation()
+        {
+            listBoxHWInfo.Items.Clear();
+        }
+    }
+}
